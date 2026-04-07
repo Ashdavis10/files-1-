c222f1e685ee2f2784bf89975efa02381995ad9e
Ashdavis10 <asherdavis@karunya.edu.in>
2026-04-07 23:02:41 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Toaster } from 'react-hot-toast';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 
 import './styles/globals.css';
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
         </Routes>
         <Toaster />
       </div>
